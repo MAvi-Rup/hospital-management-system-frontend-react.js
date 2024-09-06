83c95bc50cbc3e4b896c297df5f6011ad3087c47
MAvi-Rup <avirupmondal19@gmail.comm>
2024-09-07 01:01:42 +0600

    ENV VARAIBLE SET

--- firebase.init.js
+++ firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3JkpwV352_iLcIkoyxpWErZFddcZy4qY",
-    authDomain: "hospital-management-syst-dcf37.firebaseapp.com",
-    projectId: "hospital-management-syst-dcf37",
-    storageBucket: "hospital-management-syst-dcf37.appspot.com",
-    messagingSenderId: "721672938094",
-    appId: "1:721672938094:web:8dc2a195558de30639bc7f"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
